d99ef36e78fafaebebd4c179b6b868e6d43b6f23
ozan isgor <ozanisgor11@gmail.com>
2022-07-08 13:34:30 +0300

    add: campground 'new' & 'create'

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// req.body parser
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.render('home');
 });
@@ -23,6 +26,17 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render('campgrounds/index', { campgrounds });
+});
+
+// order does matter here.
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get('/campgrounds/:id', async (req, res) => {
