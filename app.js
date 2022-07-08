4eae5e9348ab67510f661d83df81ad490864ddf8
ozan isgor <ozanisgor11@gmail.com>
2022-07-08 11:46:38 +0300

    add: campground show

--- app.js
+++ app.js
@@ -19,9 +19,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
   res.render('home');
 });
+
 app.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render('campgrounds/show', { campground });
 });
 
 app.listen(3000, () => {
