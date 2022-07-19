06a0234dc17d2640d6fb3c452b04b118b5a79b82
ozan isgor <ozanisgor11@gmail.com>
2022-07-19 22:03:52 +0300

    update: "register" route

--- routes/users.js
+++ routes/users.js
@@ -10,13 +10,16 @@ router.get('/register', (req, res) => {
 
 router.post(
   '/register',
-  catchAsync(async (req, res) => {
+  catchAsync(async (req, res, next) => {
     try {
       const { username, email, password } = req.body;
       const user = new User({ username, email });
       const registeredUser = await User.register(user, password);
-      req.flash('success', 'Welcome to YelpCamp ' + registeredUser.username);
-      res.redirect('/campgrounds');
+      req.login(registeredUser, err => {
+        if (err) return next(err);
+        req.flash('success', 'Welcome to YelpCamp ' + registeredUser.username);
+        res.redirect('/campgrounds');
+      });
     } catch (e) {
       req.flash('error', e.message);
       res.redirect('/register');
