4020f6958ef639b800558ae006e9bc1b8d87fc23
ozan isgor <ozanisgor11@gmail.com>
2022-07-19 21:45:39 +0300

    add: logout

--- routes/users.js
+++ routes/users.js
@@ -33,4 +33,14 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
   res.redirect('/campgrounds');
 });
 
+router.get('/logout', (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      next(err);
+    }
+    req.flash('success', 'Logged you out!');
+    res.redirect('/campgrounds');
+  });
+});
+
 module.exports = router;
