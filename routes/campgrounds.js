09b66c61af207c39e8cde4a4e06c400eb37ae07c
ozan isgor <ozanisgor11@gmail.com>
2022-08-01 10:24:34 +0300

    add: authorization middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,21 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { campgroundSchema } = require('../schemas');
 const catchAsync = require('../utils/catchAsync');
-const { isLoggedIn } = require('../middleware');
+const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
 
-const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
-
-const validateCampground = (req, res, next) => {
-  const { error } = campgroundSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map(el => el.message).join(', ');
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
 
 router.get(
   '/',
@@ -61,6 +49,7 @@ router.get(
 router.get(
   '/:id/edit',
   isLoggedIn,
+  isAuthor,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
@@ -68,10 +57,7 @@ router.get(
       req.flash('error', 'Campground not found');
       return res.redirect('/campgrounds');
     }
-    if (!campground.author.equals(req.user._id)) {
-      req.flash('error', 'You are not authorized to edit this campground');
-      return res.redirect(`/campgrounds/${id}`);
-    }
+
     res.render('campgrounds/edit', { campground });
   })
 );
@@ -79,15 +65,12 @@ router.get(
 router.put(
   '/:id',
   isLoggedIn,
+  isAuthor,
   validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if (!campground.author.equals(req.user._id)) {
-      req.flash('error', 'You are not authorized to edit this campground');
-      return res.redirect(`/campgrounds/${id}`);
-    }
-    const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     req.flash('success', 'Campground updated successfully');
     res.redirect(`/campgrounds/${campground._id}`);
   })
@@ -96,6 +79,7 @@ router.put(
 router.delete(
   '/:id',
   isLoggedIn,
+  isAuthor,
   catchAsync(async (req, res) => {
     await Campground.findByIdAndDelete(req.params.id);
     req.flash('success', 'Campground deleted successfully');
