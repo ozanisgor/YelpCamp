a7fadb6d20cb88b53b398fe8d28c577184649815
ozan isgor <ozanisgor11@gmail.com>
2022-08-07 13:57:55 +0300

    add: storing uploaded image links in Mongo

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -7,14 +7,7 @@ const multer = require('multer');
 const { storage } = require('../cloudinary');
 const upload = multer({ storage });
 
-router
-  .route('/')
-  .get(catchAsync(campgrounds.index))
-  // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-  .post(upload.array('image'), (req, res) => {
-    console.log(req.body, req.files);
-    res.send('Uploaded');
-  });
+router.route('/').get(catchAsync(campgrounds.index)).post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
 
 // order does matter here.
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
