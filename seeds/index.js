01bf628b88eea74957f31852d3ed13847b558f2c
ozan isgor <ozanisgor11@gmail.com>
2022-08-07 14:32:19 +0300

    fix: seeds

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,22 @@ const seedDB = async () => {
       author: '62d6c0c7c7d659fa282c6ca9',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://source.unsplash.com/collection/483251',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      price: Math.floor(Math.random() * 20) + 10
+      price: Math.floor(Math.random() * 20) + 10,
+      images: [
+        {
+          url: 'https://res.cloudinary.com/dvakrjz9o/image/upload/v1659870807/YelpCamp/olvnrey3ptpvf7jj41qo.jpg',
+          filename: 'YelpCamp/olvnrey3ptpvf7jj41qo'
+        },
+        {
+          url: 'https://res.cloudinary.com/dvakrjz9o/image/upload/v1659870813/YelpCamp/zl1zbiej96t49pehl5dr.jpg',
+          filename: 'YelpCamp/zl1zbiej96t49pehl5dr'
+        },
+        {
+          url: 'https://res.cloudinary.com/dvakrjz9o/image/upload/v1659870818/YelpCamp/nulnrisq7n5uepqfy5sd.jpg',
+          filename: 'YelpCamp/nulnrisq7n5uepqfy5sd'
+        }
+      ]
     });
     await camp.save();
   }
