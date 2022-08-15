e93382ed170da0d239922e0709478c85c51ca41e
ozan isgor <ozanisgor11@gmail.com>
2022-08-15 12:59:57 +0300

    fix: seeds bug after adding geometry

--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,10 @@ const seedDB = async () => {
       title: `${sample(descriptors)} ${sample(places)}`,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       price: Math.floor(Math.random() * 20) + 10,
+      geometry: {
+        type: 'Point',
+        coordinates: [26.693735, 39.31726]
+      },
       images: [
         {
           url: 'https://res.cloudinary.com/dvakrjz9o/image/upload/v1659870807/YelpCamp/olvnrey3ptpvf7jj41qo.jpg',
