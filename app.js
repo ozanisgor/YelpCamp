bbdb3195cc7ecd58dc449584036df6a82be24220
ozan isgor <ozanisgor11@gmail.com>
2022-08-16 15:10:23 +0300

    add: mongo for session store

--- app.js
+++ app.js
@@ -19,10 +19,14 @@ const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const { log } = require('console');
 
-// connect to the database as development or production. For production change to DB_URL
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+const MongoDBStore = require('connect-mongo')(session);
+
+// const dbUrl = process.env.DB_URL;
+const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
@@ -48,7 +52,18 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
 
+const store = new MongoDBStore({
+  url: dbUrl,
+  secret: 'thisshouldbeabettersecret',
+  touchAfter: 24 * 60 * 60
+});
+
+store.on('error', function (e) {
+  console.log('Session Store Error', e);
+});
+
 const sessionConfig = {
+  store,
   name: 'session',
   secret: 'thisshouldbeabettersecret',
   resave: false,
