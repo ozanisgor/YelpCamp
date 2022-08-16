0bacde089216c763270c2c9c3221925d0e6183cb
ozan isgor <ozanisgor11@gmail.com>
2022-08-16 14:52:47 +0300

    add: set up Mongo Atlas

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
+// connect to the database as development or production. For production change to DB_URL
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
   useCreateIndex: true,
